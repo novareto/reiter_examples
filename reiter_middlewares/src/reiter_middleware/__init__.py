8cdb8ad42769d13962c18ee2897acee350fbbe91
Souheil Chelfouh <trollfot@gmail.com>
2021-06-09 16:10:58 +0200

    Corrected content type

--- reiter_middlewares/src/reiter_middleware/__init__.py
+++ reiter_middlewares/src/reiter_middleware/__init__.py
@@ -2,6 +2,7 @@ from roughrider.routing.route import Routes
 from horseman.response import reply
 from fanstatic import Library, Resource
 
+
 library = Library('reiter_middleware', 'static')
 css = Resource(library, 'style.css')
 
@@ -12,4 +13,7 @@ routes = Routes()
 @routes.register('/', methods=['GET'])
 def index(request):
     css.need()
-    return reply(200, "<html> <head> </head> <body>Hello world</body> </html>", {'content_type': 'text/html'})
+    return reply(
+        200, "<html><head></head><body>Hello world</body></html>",
+        {'Content-Type': 'text/html'}
+    )
